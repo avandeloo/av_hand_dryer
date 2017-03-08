45a53d8b9b04dfbe7feb6b6944177dec4e343df2
avandeloo <anthony.vandeloo@gmail.com>
2017-03-07 19:14:19 -0600

    initial commit

---
+++ lib/av_hand_dryer.rb
@@ -0,0 +1,10 @@
+require "av_hand_dryer/version"
+
+module AvHandDryer
+  class Dryer
+    def turn_on
+      `say ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo`
+      
+    end
+  end
+end
